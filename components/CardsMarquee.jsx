4cb7e38d012ba64716483cc185ceeeb2ce51bfc6
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-11-14 16:05:47 +0600

    Hero section done

--- components/CardsMarquee.jsx
+++ components/CardsMarquee.jsx
@@ -1,13 +1,25 @@
 import Marquee from "react-fast-marquee";
-import images from "@/json/marqueeImage.json"
+import images from "@/json/marqueeImage.json";
 import Image from "next/image";
 
-export default function CardsMarquee({ direction }) {
-    return (
-        <Marquee speed={30} autoFill={true} className="overflow-visible" direction={direction ? direction : "left"}>
-            {images.map((image) =>
-                <Image height={800} width={450} key={image.id} src={image.image} alt={image.title} className="max-w-80 mx-3 rounded-xl" />
-            )}
-        </Marquee>
-    )
+export default function CardsMarquee({ direction, className }) {
+  return (
+    <Marquee
+      speed={30}
+      autoFill={true}
+      className={`!absolute rotate-80 -z-10 ${className ? className : ""}`}
+      direction={direction ? direction : "left"}
+    >
+      {images.map((image) => (
+        <Image
+          height={800}
+          width={450}
+          key={image.id}
+          src={image.image}
+          alt={image.title}
+          className="max-w-56 mx-3 rounded-xl"
+        />
+      ))}
+    </Marquee>
+  );
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,33 +1,32 @@
 import CardsMarquee from "./CardsMarquee";
+import MarketingMarquee from "./MarketingMarquee";
 import Button from "./ui/Button";
+import { IoIosArrowForward } from "react-icons/io";
 
 export default function Hero() {
-    return (
-        <div className="hero min-h-screen relative overflow-hidden">
-            {/* <MarketingMarquee /> */}
-            <div className="h-52 w-60 bg-red-500 absolute rotate-80 right-0"></div>
-            <div className="absolute rotate-80 -z-10 grid grid-rows-2 gap-5 overflow-hidden">
-                <div>
-                    <CardsMarquee />
-                </div>
-                <div>
-                    <CardsMarquee direction="right" />
-                </div>
-            </div>
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                    className="max-w-sm rounded-lg"
-                />
-                <div>
-                    <h1 className="text-5xl font-bold text-balance">Creative strategy that drives real results</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                    <Button>Get Started</Button>
-                </div>
-            </div>
+  return (
+    <div className="py-44 relative overflow-hidden flex flex-col justify-center">
+      <div className="h-52 absolute top-0 bg-linear-to-t from-transparent to-gray-950 w-full"></div>
+      <MarketingMarquee />
+      <CardsMarquee className="-right-[200px]" />
+      <CardsMarquee direction="right" className="-right-[565px]" />
+
+      <div className="p-5 max-w-[1426px] mx-auto w-full">
+        <div className="max-w-xl">
+          <h1 className="text-5xl font-bold text-balance">
+            Creative strategy that drives real results
+          </h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <Button>Get Started</Button>
+          <button className="btn btn-lg btn-link no-underline text-white hover:text-main shadow-none">
+            Portfolio <IoIosArrowForward className="size-5 text-main" />
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
