af2eac66487a52c0e5747b5fec0579b0ac76144e
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-11-20 00:11:21 +0600

    restricted logged in user to access auth forms

--- app/register/page.js
+++ app/register/page.js
@@ -1,6 +1,30 @@
+"use client";
+
 import AuthPage from "@/components/auth/AuthPage";
+import Loader from "@/components/ui/Loader";
+import auth from "@/firebase/firebase.config";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Register = () => {
+  const router = useRouter();
+  const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (!loading && user) {
+      router.push("/dashboard");
+    }
+  }, [user, loading, router]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (user) {
+    return <Loader />;
+  }
+
   return <AuthPage login={false} />;
 };
 
