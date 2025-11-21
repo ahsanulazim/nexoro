18e98a8cfc6a50775a0914353e186479188f2911
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-11-21 21:39:07 +0600

    terms and conditions done

--- app/(main)/terms-and-conditions/page.js
+++ app/(main)/terms-and-conditions/page.js
@@ -1,5 +1,6 @@
 import GradText from "@/components/ui/GradText"
 import TitleBanner from "@/components/ui/TitleBanner"
+import { LuMail, LuPhone } from "react-icons/lu"
 
 const page = () => {
     return (
@@ -12,10 +13,10 @@ const page = () => {
                     </h2>
                     <p className="text-center"><b>Last Updated:</b> 20 November 2025</p>
                 </div>
-                <div className="bg-base-300 p-5 rounded-xl">
+                <div className="bg-base-300 p-5 md:p-10 rounded-xl">
                     <p>Welcome to Nexoro Solutions, a professional digital marketing agency providing a wide range of digital services. By accessing our website and purchasing our services, you agree to the following Terms & Services.</p>
                     <br />
-                    <ol className="pl-5 *:list-decimal *:text-2xl *:font-semibold">
+                    <ol className="pl-10 *:list-decimal *:text-2xl *:font-semibold">
                         <li>Services We Provide
                             <ul className="*:font-normal *:text-base *:list-disc text-gray-400">
                                 <li>Social Media Management</li>
@@ -29,6 +30,41 @@ const page = () => {
                         <br />
                         <li>Client Responsibilities
                             <p className="font-normal text-base text-gray-400">Clients must provide accurate information, submit necessary materials, respond on time, and follow professional communication.</p>
+                        </li>
+                        <br />
+                        <li>Payment Terms
+                            <p className="font-normal text-base text-gray-400">Full payment must be made before project start. Monthly services require advance payment. Additional custom work requires extra charges.</p>
+                        </li>
+                        <br />
+                        <li> Revisions
+                            <p className="font-normal text-base text-gray-400">Packages include limited revisions. Extra revisions cost additional fees. Revisions do not include redesign unless agreed.</p>
+                        </li>
+                        <br />
+                        <li> Project Delivery Timeline
+                            <p className="font-normal text-base text-gray-400">Timeline begins after receiving all required materials. Client delays extend delivery.</p>
+                        </li>
+                        <br />
+                        <li>Intellectual Property Rights
+                            <p className="font-normal text-base text-gray-400">Final design rights transfer to client upon full payment. Source files provided only if included or purchased. Nexoro may use work in portfolio unless requested otherwise.</p>
+                        </li>
+                        <br />
+                        <li> Confidentiality
+                            <p className="font-normal text-base text-gray-400">All client information remains confidential.</p>
+                        </li>
+                        <br />
+                        <li> Termination of Services
+                            <p className="font-normal text-base text-gray-400">Service may be terminated for unpaid dues, violations, abuse, or unresponsiveness. No refund in such cases.</p>
+                        </li>
+                        <br />
+                        <li> Limitation of Liability
+                            <p className="font-normal text-base text-gray-400">Nexoro Solutions is not responsible for platform issues, copyrighted materials from clients, or business losses.</p>
+                        </li>
+                        <br />
+                        <li> Contact Information
+                            <ul className="*:font-normal *:text-base *:text-gray-400">
+                                <li className="flex items-center gap-2"><LuMail /> your-email@example.com</li>
+                                <li className="flex items-center gap-2"><LuPhone /> your-number</li>
+                            </ul>
                         </li>
                     </ol>
                 </div>
