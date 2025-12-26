2ab6e372f03f6aa410d02bf1c997136fb4c2077d
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-12-26 23:38:29 +0600

    logo slider add remove done 2

--- components/dashboard/sliders/SliderTab.jsx
+++ components/dashboard/sliders/SliderTab.jsx
@@ -4,14 +4,8 @@ const SliderTab = () => {
     return (
         <>
             {/* name of each tab group should be unique */}
-            <div className="tabs tabs-box">
+            <div className="tabs tabs-lift">
                 <input type="radio" name="my_tabs_6" className="tab" aria-label="Client Logos" defaultChecked />
-                <div className="tab-content bg-base-100 border-base-300 p-6"><SliderContent /></div>
-
-                <input type="radio" name="my_tabs_6" className="tab" aria-label="Services" />
-                <div className="tab-content bg-base-100 border-base-300 p-6"><SliderContent /></div>
-
-                <input type="radio" name="my_tabs_6" className="tab" aria-label="Reviews" />
                 <div className="tab-content bg-base-100 border-base-300 p-6"><SliderContent /></div>
             </div>
         </>
